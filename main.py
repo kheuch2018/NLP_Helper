b1daf7db5d630b32e08778d687350f33c431bada
Cheikh Seck <cheikhseck980@gmail.com>
2021-04-17 13:43:16 +0000

    adding tagset and X

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ class Dataset:
   text_path=""
   tags_path=""
   sentences = {}
-  keys=[]
+  keys = []
   vocab = []
-
+  tagset = [] 
+  X = ()
+  Y = ()
+  
   def __init__(self,text_path,tags_path):
     self.text_path = text_path
     self.tags_path = tags_path
     self.create_sentences()
     self.create_keys()
     self.create_vocab()
+    self.create_tagset()
+    self.create_X()
+    self.create_Y()
 
   def create_sentences(self):
     sentence_key = ""
@@ -35,7 +41,21 @@ class Dataset:
     for tup in self.sentences.values() :
       for word in tup :
         my_vocab[word] = True
-    self.vocab = my_vocab.keys()  
+    self.vocab = my_vocab.keys() 
+  
+  def create_tagset(self):
+    tags = []
+    f = open(self.tags_path,"r")
+    for line in f.readlines():
+      tags.append(line.replace("\n",""))
+    else:
+      self.tagset = tags
+  
+  def create_X(self):
+    self.X = tuple(self.sentences.values())
+  
+
+     
 
 
 dataset = Dataset("./text.txt","tags.txt")
