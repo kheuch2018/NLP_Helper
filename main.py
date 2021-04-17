68a084d42f207d227c7e130b6ec72e481538f3ef
Cheikh Seck <cheikhseck980@gmail.com>
2021-04-17 12:52:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+class Dataset:
+  text_path=""
+  tags_path=""
+  sentences = {}
+  def __init__(self,text_path,tags_path):
+    self.text_path = text_path
+    self.tags_path = tags_path
+    self.create_sentences()
+
+  def create_sentences(self):
+    sentence_key = ""
+    sentence = ()
+    f = open(self.text_path,"r")
+    for line in f.readlines():
+      if "wo_" in line :
+        sentence = ()
+        sentence_key = line.replace("\n","")
+      elif line !="\n" :
+        sentence = sentence + (line.replace("\n","").split("\t")[0],)
+      elif line == "\n" and len(sentence) :
+        self.sentences[sentence_key] = sentence
+    else:
+      self.sentences[sentence_key] = sentence
+  
+  
+
+
+dataset = Dataset("./text.txt","tags.txt")
+
+  
